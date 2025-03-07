801ba20d603e14bca67915e5d6aa876488c6849e
Asad Shaikh <shaikhsaime02@gmail.com>
2025-03-07 23:26:07 +0530

    avtar update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
           </div>
           <div className="flex-1">
             <img
-              src="/images/myavtar.png"
+              src="/images/awsq.png"
               alt="Developer workspace"
               className="rounded-2xl shadow-2xl"
             />
